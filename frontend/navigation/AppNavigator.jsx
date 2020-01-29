5da5159351a9a34c2d4996339d0b292f450d57ff
KennethOyibo <k.oyibo@ufl.edu>
2020-01-29 14:41:19 -0500

    removed header in homescreen navigation

--- frontend/navigation/AppNavigator.jsx
+++ frontend/navigation/AppNavigator.jsx
@@ -1,18 +1,26 @@
-import { createStackNavigator} from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 
 
-const AppNavigator = createStackNavigator({
-    Home: HomeScreen,
-    //another way of creating stack navigation, gives you more access
-    //to props
-    Login: {
-        screen: LoginScreen
+const AppNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                headerShown: false
+            }
+        },
+        Login: {
+            screen: LoginScreen,
+            navigationOptions: {
+                headerShown: true
+            }
+        }
     }
-});
+);
 
 
 export default createAppContainer(AppNavigator);
