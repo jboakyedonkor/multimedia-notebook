3093ed018674b715f78415c7ba5fc6058afe77c4
Jesse Boakye-Donkor <jes142dd@ufl.edu>
2020-03-15 00:13:25 -0400

    add new settings dependencies

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8-alpine
-
-EXPOSE 8000
-COPY backend/ backend/
-COPY requirements.txt backend/requirements.txt
-WORKDIR backend/
-RUN pip install -r requirements.txt && python manage.py collectstatic && \
-python manage.py migrate 
-ENTRYPOINT [ "gunicorn","-b","0.0.0.0:8000","backend.wsgi"]
--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -16,7 +16,6 @@ dotenv.load_dotenv('.'+os.sep+'app.env')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
