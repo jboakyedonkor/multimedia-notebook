e31ca22066f76fe2b7403b87d37b4181a9a6e6dd
Jesse Boakye-Donkor <jes142dd@ufl.edu>
2020-03-15 20:39:26 -0400

    turned debug on

--- backend/backend/settings/deployment.py
+++ backend/backend/settings/deployment.py
@@ -24,7 +24,7 @@ print(os.getcwd())
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['localhost','0.0.0.0','127.0.0.1']
